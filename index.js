3437ca6221b5a696aaa2a443b842513a341dcd92
김남우 <121301972+KNamWoo@users.noreply.github.com>
2024-09-23 12:12:50 +0900

    update

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function first() {
-    location.href="./01week/";
+    location.href="./01week/index.html";
+}
+
+function second() {
+    location.href="./02week/02-20230056-김남우-1.html";
+}
+
+function third() {
+    location.href="./01week/실습-1.html";
+}
+
+function fourth() {
+    location.href="./01week/index.html";
 }
